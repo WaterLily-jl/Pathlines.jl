4f7b79b868a2f6686960d0b8e4b6dea2ea1cbb97
pquesadaallerh <p.quesadallerhand@tudelft.nl>
2025-09-05 13:42:16 +0200

    add custom minsize and width settings to ParticleViz

--- src/ParticleViz.jl
+++ src/ParticleViz.jl
@@ -3,21 +3,27 @@ struct ParticleViz
     pos; opos
     pmag; mag; omag
     pdir; dir; odir
+    scale::Float64
+    minsize::Float64
+    width::Float64
 end
-function ParticleViz(p::Particles,Δt)
+
+function ParticleViz(p::Particles, Δt; scale=20, minsize=2, width=2)
     pos = p.position |> Array; opos = GLMakie.Observable(pos)
-    pmag = velmag.(p.position,p.position⁰,Δt)
+    pmag = velmag.(p.position, p.position⁰, Δt; scale=scale, minsize=minsize, width=width)
     mag = pmag |> Array; omag = GLMakie.Observable(mag)
-    pdir = veldir.(p.position,p.position⁰)
+    pdir = veldir.(p.position, p.position⁰)
     dir = pdir |> Array; odir = GLMakie.Observable(dir)
-    ParticleViz(pos,opos,pmag,mag,omag,pdir,dir,odir) 
+    ParticleViz(pos, opos, pmag, mag, omag, pdir, dir, odir, scale, minsize, width)
 end
-function notify!(v::ParticleViz,p::Particles,Δt)
-    copyto!(v.pos,p.position); notify(v.opos)
-    v.pmag .= velmag.(p.position,p.position⁰,Δt)
-    copyto!(v.mag,v.pmag); notify(v.omag)
-    v.pdir .= veldir.(p.position,p.position⁰)
-    copyto!(v.dir,v.pdir); notify(v.odir)
+
+function notify!(v::ParticleViz, p::Particles, Δt)
+    copyto!(v.pos, p.position); notify(v.opos)
+    v.pmag .= velmag.(p.position, p.position⁰, Δt; scale=v.scale, minsize=v.minsize, width=v.width)
+    copyto!(v.mag, v.pmag); notify(v.omag)
+    v.pdir .= veldir.(p.position, p.position⁰)
+    copyto!(v.dir, v.pdir); notify(v.odir)
 end
-velmag(x,x0,Δt) = (dx=x-x0; GLMakie.Vec2f(2+20√(dx'*dx)/Δt,2))
-veldir(x,x0) = (dx=x-x0; atan(dx[2],dx[1]))
+
+velmag(x, x0, Δt; scale=20, minsize=2, width=2) = (dx = x - x0; GLMakie.Vec2f(minsize + scale * √(dx' * dx) / Δt, width))
+veldir(x, x0) = (dx = x - x0; atan(dx[2], dx[1]))

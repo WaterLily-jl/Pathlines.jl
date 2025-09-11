f2bea9a626786829700c28e8f9351bc87873f75a
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2025-09-11 18:20:59 +0200

    Suggested fixes

--- examples/ParticlePlot.jl
+++ examples/ParticlePlot.jl
@@ -1,21 +1,18 @@
-using WaterLily,CUDA,Pathlines
-import GLMakie
+using WaterLily,CUDA,Pathlines,GLMakie
+
 function make_particleplot(;N=Int(1e4),life=UInt(255),name="particleplot.mp4",U=1,L=64,T=Float32,mem=CUDA.CuArray)
+    k = T(π/L)
     function uλ(i,xy)
-        x,y = @. (xy-1.5)*π/L           # scaled coordinates
-        if i==1 
-            return T(-U*sin(x)*cos(y))   # u_x
-        elseif i==2 
-            return T(U*cos(x)*sin(y))    # u_y
-        else
-            return T(0)                  # default case
-        end
+        x,y = @. (xy-1.5f0)*k            # scaled coordinates
+        i==1 && return -U*sin(x)*cos(y)  # u_x
+        i==2 && return  U*cos(x)*sin(y)  # u_y
+        zero(k)
     end
     sim = Simulation((L,L),(0, 0), L; U, uλ, ν=L*U/1e3, T, mem)
 
     # Set up Particles and CPU visualization arrays
     p = Particles(N,sim.flow.σ;mem,life)
-    v = ParticleViz(p,sim.flow.Δt[1])
+    v = ParticleViz(p,sim.flow.Δt[1],width=1)
 
     #Set up figure
     fig = GLMakie.Figure()
@@ -25,12 +22,11 @@ function make_particleplot(;N=Int(1e4),life=UInt(255),name="particleplot.mp4",U=
     # Record video
     @time GLMakie.record(fig,name,0.01:0.05:2.0) do t
         while sim_time(sim)<t
-            WaterLily.mom_step!(sim.flow,sim.pois)
-            update!(p,sim)
+            sim_step!(sim)
+            Pathlines.update!(p,sim)
         end
         notify!(v,p,sim.flow.Δt[end-1])
     end
 end
-
 
 make_particleplot()
--- src/ParticleViz.jl
+++ src/ParticleViz.jl
@@ -3,27 +3,26 @@ struct ParticleViz
     pos; opos
     pmag; mag; omag
     pdir; dir; odir
-    scale::Float64
-    minsize::Float64
-    width::Float64
+    velmag::Function
 end
 
 function ParticleViz(p::Particles, Δt; scale=20, minsize=2, width=2)
     pos = p.position |> Array; opos = GLMakie.Observable(pos)
-    pmag = velmag.(p.position, p.position⁰, Δt; scale=scale, minsize=minsize, width=width)
+    vizvelmag(x,x0,Δt) = velmag(x,x0,Δt; scale, minsize, width)
+    pmag = vizvelmag.(p.position, p.position⁰, Δt)
     mag = pmag |> Array; omag = GLMakie.Observable(mag)
     pdir = veldir.(p.position, p.position⁰)
     dir = pdir |> Array; odir = GLMakie.Observable(dir)
-    ParticleViz(pos, opos, pmag, mag, omag, pdir, dir, odir, scale, minsize, width)
+    ParticleViz(pos, opos, pmag, mag, omag, pdir, dir, odir, vizvelmag)
 end
 
 function notify!(v::ParticleViz, p::Particles, Δt)
     copyto!(v.pos, p.position); notify(v.opos)
-    v.pmag .= velmag.(p.position, p.position⁰, Δt; scale=v.scale, minsize=v.minsize, width=v.width)
+    v.pmag .= v.velmag.(p.position, p.position⁰, Δt)
     copyto!(v.mag, v.pmag); notify(v.omag)
     v.pdir .= veldir.(p.position, p.position⁰)
     copyto!(v.dir, v.pdir); notify(v.odir)
 end
 
-velmag(x, x0, Δt; scale=20, minsize=2, width=2) = (dx = x - x0; GLMakie.Vec2f(minsize + scale * √(dx' * dx) / Δt, width))
+velmag(x, x0, Δt; scale=20, minsize=2, width=2) = (dx = x - x0; GLMakie.Vec2f(minsize+scale*√(dx'*dx)/Δt,width))
 veldir(x, x0) = (dx = x - x0; atan(dx[2], dx[1]))

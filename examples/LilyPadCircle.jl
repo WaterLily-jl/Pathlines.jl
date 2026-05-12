3a0d8a4b4b798a24443261957e41f4a7740ee38e
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2026-05-12 10:18:02 +0200

    update circle demo

--- examples/LilyPadCircle.jl
+++ examples/LilyPadCircle.jl
@@ -1,9 +1,10 @@
 using WaterLily, LilyPad, StaticArrays, BiotSavartBCs, CUDA, Pathlines, GLMakie
 import ColorSchemes: colorschemes
 
-function make_lilypad_circle(; N=Int(2e4), life=UInt(200), Δt=1.5, T=Float32,
-                               mem=CUDA.functional() ? CUDA.CuArray : Array)
-    n, m = 2^8, 2^7   # 256×128
+function make_lilypad_circle(; N=2_000, life=UInt(200), Δt=1.5, T=Float32,
+                               mem=CUDA.functional() ? CUDA.CuArray : Array,
+                               colormap=:starrynight, p=7)
+    n, m = 2^(p+1), 2^p
     radius, center, ω = T(m / 8), SVector{2,T}(m/2-1, m/2-1), zero(T)
 
     body=AutoBody((x, t) -> √(x'x) - radius,
@@ -11,21 +12,22 @@ function make_lilypad_circle(; N=Int(2e4), life=UInt(200), Δt=1.5, T=Float32,
 
     sim = LilyBiotSim((n, m), (1, 0), 2radius; ν=0, Δt, body, mem, T)
 
-    bgcolor = colorschemes[:starrynight].colors[1]
-    bodycolor = colorschemes[:starrynight].colors[end]
+    bgcolor = colorschemes[colormap].colors[1]
+    bodycolor = colorschemes[colormap].colors[end]
     viz = PathViz(sim; N, life, mem, body=true, bgcolor, figsize=(1024, 512),
-                  colormap=:starrynight, colorrange=(0, 2), bodycolor)
+                  colormap, colorrange=(0, 2), bodycolor)
 
-    function points(θ) 
+    function points(θ) # endpoints of a spoke at angle θ
         r = radius*SVector{2, T}(cos(θ), sin(θ))
-        c = center .+ 0.5
+        c = center .+ T(0.5)
         [Point2f((c+r)...),Point2f((c-r)...)]
     end
     return sim,viz,points
 end
 
 begin
-    sim,viz,points = make_lilypad_circle(N=2_000);
+    # Create the simulation and visualization objects, and display the figure
+    sim,viz,points = make_lilypad_circle()
     Pathlines.step!(viz, sim, 0.1; remeasure=false)
     θ = Ref(0f0); dt = 0.1f0
     spoke = Observable(points(θ[]))
@@ -33,7 +35,8 @@ begin
     display(viz.fig)
 end
 
-begin
+ begin
+    # Control the rotation-rate of the circle with the up/down arrow keys, and space to stop
     ω = Ref(0f0); dω = Float32(sim.U/sim.L)
     on(events(viz.fig).keyboardbutton) do event
         event.action == Keyboard.press || return
@@ -45,6 +48,7 @@ begin
         measure!(sim)
     end
 
+    # Update the sim and spoke endpoints in a loop until the Makie window is closed
     while events(viz.fig).window_open[]
         t = sim_time(sim)+dt
         Pathlines.step!(viz, sim, t; remeasure = false)
@@ -52,4 +56,4 @@ begin
         spoke[] = points(θ[])
         yield()   # hand control back to GLMakie event loop
     end
-end
+end 
